2a11d3f950892990020608890ca8af8602f0a428
ertemann <info@crypticcurve.com>
2026-03-02 23:13:49 +0100

    remove iframeApp components, we now use the main app wrapped in a frame with searchparam

--- src/components/IframeApp/hooks/index.ts
+++
@@ -1,4 +0,0 @@
-export { useIframeMessageHandler } from "./useIframeMessageHandler";
-export { useIframeModals, type ModalType } from "./useIframeModals";
-export { useIframeSession } from "./useIframeSession";
-export { useModalStyling } from "./useModalStyling";
